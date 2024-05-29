891cbdacbbd78924ce1009c46d1f47f9e3b44f6d
yukwangsu <86365727+yukwangsu@users.noreply.github.com>
2024-05-30 00:40:16 +0900

    Episodes title, button

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -48,75 +48,125 @@ class _DetailScreenState extends State<DetailScreen> {
             ),
           ),
         ),
-        body: Column(
-          children: [
-            const SizedBox(
-              height: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(50),
+            child: Column(
               children: [
-                //webtoon_widet과 동일한 태그를 가지면 화면전환이 자연스럽게 된다.
-                Hero(
-                  tag: widget.id,
-                  child: Container(
-                    width: 250,
-                    clipBehavior: Clip.hardEdge,
-                    //container 장식
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          //blurRadius: shadow 크기
-                          blurRadius: 15,
-                          offset: const Offset(10, 10),
-                          color: Colors.black.withOpacity(0.5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //webtoon_widet과 동일한 태그를 가지면 화면전환이 자연스럽게 된다.
+                    Hero(
+                      tag: widget.id,
+                      child: Container(
+                        width: 250,
+                        clipBehavior: Clip.hardEdge,
+                        //container 장식
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              //blurRadius: shadow 크기
+                              blurRadius: 15,
+                              offset: const Offset(10, 10),
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                          ],
                         ),
-                      ],
+                        //container 내용
+                        child: Image.network(
+                          widget.thumb,
+                          headers: const {
+                            'Referer': 'https://comic.naver.com',
+                          },
+                        ),
+                      ),
                     ),
-                    //container 내용
-                    child: Image.network(
-                      widget.thumb,
-                      headers: const {
-                        'Referer': 'https://comic.naver.com',
-                      },
-                    ),
-                  ),
+                  ],
                 ),
+                const SizedBox(
+                  height: 25,
+                ),
+                FutureBuilder(
+                  future: webtoon,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            snapshot.data!.about,
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            //한번에 여러 정보를 text로 보여주기
+                            '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      );
+                    }
+                    return const Text("...");
+                  },
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                FutureBuilder(
+                  future: episodes,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        children: [
+                          for (var episode in snapshot.data!.length > 10
+                              ? snapshot.data!.sublist(0, 10)
+                              : snapshot.data!)
+                            Container(
+                              //margin: container 사이에 간격을 추가
+                              margin: const EdgeInsets.only(bottom: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.green.shade400,
+                              ),
+                              child: Padding(
+                                //container 내부에 padding을 추가
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 20,
+                                ),
+                                child: Row(
+                                  //양쪽 끝에 배치
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      episode.title,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const Icon(
+                                      Icons.chevron_right_rounded,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                        ],
+                      );
+                    }
+                    return Container();
+                  },
+                )
               ],
             ),
-            const SizedBox(
-              height: 25,
-            ),
-            FutureBuilder(
-              future: webtoon,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          snapshot.data!.about,
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          //한번에 여러 정보를 text로 보여주기
-                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-                return const Text("...");
-              },
-            )
-          ],
+          ),
         ));
   }
 }
