fe3fa05111d8b8f0a225917d5c741ef4f601e09c
yukwangsu <86365727+yukwangsu@users.noreply.github.com>
2024-05-30 00:02:30 +0900

    detail info

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -85,6 +85,37 @@ class _DetailScreenState extends State<DetailScreen> {
                 ),
               ],
             ),
+            const SizedBox(
+              height: 25,
+            ),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          //한번에 여러 정보를 text로 보여주기
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return const Text("...");
+              },
+            )
           ],
         ));
   }
